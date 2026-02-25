a7376d4b0376778a4fa86e71add431ba0cc07f8c
walmirferjr <walmir.jr.2004@usp.br>
2026-02-25 03:02:56 -0300

    test: testando erro ao mostrar certificações na vercel

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -23,11 +23,11 @@ export default function Certifications() {
           Certificações
         </motion.h2>
         <motion.p
-          className="section-subtitle mb-10"
-          initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true }}
-          transition={{ duration: 0.6, delay: 0.1 }}
+       //   className="section-subtitle mb-10"
+       //   initial={{ opacity: 0, y: 20 }}
+       //   whileInView={{ opacity: 1, y: 0 }}
+       //   viewport={{ once: true }}
+       //   transition={{ duration: 0.6, delay: 0.1 }}
         >
           Credenciais e links para verificação.
         </motion.p>
